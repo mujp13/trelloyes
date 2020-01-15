afc90b27a552228d1d3db7d9bc1235b54a271ff4
Kevin Park <keunsuk413@gmail.com>
2020-01-15 16:10:58 -0600

    3rd commit

--- src/App.js
+++ src/App.js
@@ -3,31 +3,31 @@ import List from './List';
 import './App.css';
 
 
-class App extends Component() {
+class App extends Component {
   static defaultProps = {
     store: {
       lists: [],
-      allCards: {}
+      allCards: {},
     }
   };
-  
+
   render() {
-    const {store} = this.props;
+    const { store } = this.props
     return (
-      <main className="App">
-        <header className="App-header">
+      <main className='App'>
+        <header className='App-header'>
           <h1>Trelloyes!</h1>
         </header>
-      <div className="App-list">
-        {store.lists.map(list => (
-          <List
-            key={list.id}
-            header={list.header}
-            cards={list.cardIds.map(id => store.allCards[id])}
-          />
-        ))}
-      </div>
-    </main>
+        <div className='App-list'>
+          {store.lists.map(list => (
+            <List
+              key={list.id}
+              header={list.header}
+              cards={list.cardIds.map(id => store.allCards[id])}
+            />
+          ))}
+        </div>
+      </main>
     );
   }
 }
